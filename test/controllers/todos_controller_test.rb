fca6e909e4a15b0015d45d0b48e04ad8058fd48a
Xing <151417971+Mythventor@users.noreply.github.com>
2026-05-26 22:33:51 -0500

    Update todos_controller_test.rb

--- test/controllers/todos_controller_test.rb
+++ test/controllers/todos_controller_test.rb
@@ -38,6 +38,15 @@ class TodosControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to todo_url(@todo)
   end
 
+  test "should toggle priority" do
+    todo = todos(:one)
+
+    patch toggle_priority_todo_path(todo), headers: { "Accept" => "text/vnd.turbo-stream.html" }
+
+    assert_equal "text/vnd.turbo-stream.html", response.media_type
+    assert todo.reload.high_priority
+  end
+
   test "should destroy todo" do
     assert_difference("Todo.count", -1) do
       delete todo_url(@todo)
